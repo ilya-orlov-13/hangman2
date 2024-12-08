44a8580ae01fd296c44349f7447297d4b40d36bd
T-Vlasov <timurvlasow@gmail.com>
2024-12-08 05:13:56 +0500

    Функция проверки буквы

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -15,3 +15,18 @@ void displayWord(const string& word, const vector<bool>& guessed) {
     }
     cout << endl;
 }
+
+bool checkLetter(char letter, const string& word, vector<bool>& guessed) {
+    bool found = false;
+    for (int i = 0; i < word.size(); ++i) {
+        if (tolower(word[i]) == tolower(letter)) {
+            if (guessed[i]) {
+                cout << "Вы уже угадали эту букву\n";
+                return true;
+            }
+            guessed[i] = true;
+            found = true;
+        }
+    }
+    return found;
+}
